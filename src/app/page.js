c6c8df2f2639752001a81e17248e747dbfa52e25
Philip-Kwao <123540619+Philip-Kwao@users.noreply.github.com>
2023-09-11 08:07:55 +0000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="z-10 w-full items-center justify-between font-mono text-sm">
         <h1 className='text-center text-4xl font-extrabold uppercase'>driver account system</h1>
         <div className='flex flex-col  lg:flex-row justify-between items-center'>
           <Form background={"bg-green-600"} database={"myIncome"} title={"My Income"}/>
